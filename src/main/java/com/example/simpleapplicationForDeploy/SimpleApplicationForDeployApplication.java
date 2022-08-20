5a58b29b449c06f91c9441abd9554fb586d7e6b7
SSiik <sik_5571@naver.com>
2022-08-20 13:54:34 +0900

    my First commit

---
+++ src/main/java/com/example/simpleapplicationForDeploy/SimpleApplicationForDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleapplicationForDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApplicationForDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApplicationForDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleapplicationForDeploy/SimpleApplicationForDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleapplicationForDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApplicationForDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
